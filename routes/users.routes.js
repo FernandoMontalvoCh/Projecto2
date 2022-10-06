f1b8c0761367acd36988fdab9ca3253752f1ae78
FernandoMontalvoCh <fmontalvochavez@gmail.com>
2022-10-06 00:04:25 -0500

    new

---
+++ routes/users.routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+// Middlewares
+const {
+  userExists,
+  protectAccountOwner,
+} = require('../middlewares/users.middlewares');
+const { protectSession } = require('../middlewares/auth.middlewares');
+const {
+  createUserValidations,
+  checkValidations,
+} = require('../middlewares/validators.middlewares');
+
+// Controller
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  login,
+  checkToken,
+  getUserProducts,
+  getUserOrders,
+  getUserOrderById,
+} = require('../controllers/users.cotroller');
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', createUserValidations, checkValidations, createUser);
+
+usersRouter.post('/login', login);
+
+usersRouter.use(protectSession);
+
+usersRouter.get('/', getAllUsers);
+
+usersRouter.get('/me', getUserProducts);
+
+usersRouter.get('/orders', getUserOrders);
+
+usersRouter.get('/orders/:id', getUserOrderById);
+
+usersRouter.get('/check-token', checkToken);
+
+usersRouter
+  .route('/:id')
+  .get(userExists, getUserById)
+  .patch(userExists, protectAccountOwner, updateUser)
+  .delete(userExists, protectAccountOwner, deleteUser);
+
+module.exports = { usersRouter };
